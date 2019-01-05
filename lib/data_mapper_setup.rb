2c56c1b18976bbfc435ace0d5e43f744a8b35f3d
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-05 13:37:34 +0000

    Change dev database url

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -3,5 +3,5 @@ require 'dm-postgres-adapter'
 require './lib/models/user'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/whatsapp_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/whatsapp_users_#{ENV['RACK_ENV']}")
 DataMapper.finalize
