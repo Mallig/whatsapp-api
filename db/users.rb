d1b94df36a4364c9e7b553707ef1be466d4fbc8e
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-09 19:04:58 +0000

    seed user table

---
+++ db/users.rb
@@ -0,0 +1,38 @@
+users = [
+  {
+      :username => "Jon",
+      :password => "password123"
+  },
+  {
+      :username => "Toby",
+      :password => "password123"
+  },
+  {
+      :username => "Ellie",
+      :password => "password123"
+  },
+  {
+      :username => "Theresa",
+      :password => "password123"
+  },
+  {
+      :username => "Mal",
+      :password => "password123"
+  },
+  {
+      :username => "Hugo",
+      :password => "password123"
+  },
+  {
+      :username => "Johnson",
+      :password => "password123"
+  },
+  {
+      :username => "Jason",
+      :password => "password123"
+  },
+  {
+      :username => "Jimothy",
+      :password => "password123"
+  }
+]
--- lib/controllers/users.rb
+++ lib/controllers/users.rb
@@ -10,7 +10,51 @@ class UsersController < Sinatra::Base
     end
 
     get '/users' do
-        users = []
+        users = [
+            {
+                :username => "Jon",
+                :password => "password123"
+            },
+            {
+                :username => "Toby",
+                :password => "password123"
+            },
+            {
+                :username => "Ellie",
+                :password => "password123"
+            },
+            {
+                :username => "Theresa",
+                :password => "password123"
+            },
+            {
+                :username => "Mal",
+                :password => "password123"
+            },
+            {
+                :username => "Hugo",
+                :password => "password123"
+            },
+            {
+                :username => "Johnson",
+                :password => "password123"
+            },
+            {
+                :username => "Jason",
+                :password => "password123"
+            },
+            {
+                :username => "Jimothy",
+                :password => "password123"
+            }
+        ]
+
+        users.each do |user|
+            User.create(
+                :username => user[:username],
+                :password => user[:password]
+            )
+        end
         User.all(:fields => [:id, :username]).each do |user|
             users.push(translate_user(user))
         end
