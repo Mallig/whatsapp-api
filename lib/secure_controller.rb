f05922444164e825f14b035e2a2a2f29d966b49d
Hugo Sykes <hugo@denmeadfarm.co.uk>
2019-01-16 18:11:11 +0000

    Removes now pointless directory

--- lib/secure_controller.rb
+++ lib/secure_controller.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class SecureController < Sinatra::Base
     before do
-        headers "Content-Type" => 'application/json'
-        halt 403 unless request.env["Content-Type"] == 'application/json'
+        headers "Content-Type" => "application/json"
+        halt 403 unless request.env["CONTENT_TYPE"] == "application/json" || request.env["Content-Type"] == "application/json"
     end
 end
