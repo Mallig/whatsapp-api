ab2d5d840e99c41835362c10ea4e7612f6e18085
Hugo Sykes <hugo@denmeadfarm.co.uk>
2019-01-16 16:27:33 +0000

    Changes login service to be a class instead of a module

--- lib/login/login_service.rb
+++ lib/login/login_service.rb
@@ -1,19 +1,17 @@
 require './lib/users/user'
 require './lib/login/login_messages'
 
-module LoginService
-    class << self
-        def login(req)
-            user = User.first(:username => req["username"])
-            response = {}
-            unless user.nil?
-                if user[:password] == req["password"]
-                    response["message"] = LoginMessages::SUCCESS
-                else
-                    response["message"] = LoginMessages::FAILURE
-                end
+class LoginService
+    def self.login(req)
+        user = User.first(:username => req["username"])
+        response = {}
+        unless user.nil?
+            if user[:password] == req["password"]
+                response["message"] = LoginMessages::SUCCESS
+            else
+                response["message"] = LoginMessages::FAILURE
             end
-            return response
         end
+        return response
     end
 end
